51b20166572904cce3a5a114ac7821e295ca613c
R34perCH <p.tomasi@hotmail.ch>
2019-10-10 18:51:34 +0200

    fischerausflug html erweitert,javascript ergaenzt

--- HTML/JavaScript/fischerausflug.js
+++ HTML/JavaScript/fischerausflug.js
@@ -1,8 +1,56 @@
 function divfunction() {
     var x = document.getElementById("hechtdiv");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
       x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function rutefunction() {
+    var x = document.getElementById("hechtrute");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function rollefunction() {
+    var x = document.getElementById("hechtrolle");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function schnurfunction() {
+    var x = document.getElementById("hechtschnur");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function vorfachfunction() {
+    var x = document.getElementById("hechtvorfach");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function hackenfunction() {
+    var x = document.getElementById("hechthacken");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function koederfunction() {
+    var x = document.getElementById("hechtkoeder");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
   }
