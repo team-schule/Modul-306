0f3f1ab28caf88f9c2ec7f9b82f85672762f5649
R34perCH <p.tomasi@hotmail.ch>
2019-10-13 23:16:10 +0200

    Karpfenangeln JS implementiert

--- HTML/JavaScript/fischerausflug.js
+++ HTML/JavaScript/fischerausflug.js
@@ -1,4 +1,9 @@
-function divfunction() {
+//
+// Scripts für Zielfisch Hecht
+//
+
+// JavaScript Hechtbild
+function hechtdivfunction() {
     var x = document.getElementById("hechtdiv");
     if (x.style.display === "block") {
       x.style.display = "none";
@@ -6,6 +11,7 @@ function divfunction() {
       x.style.display = "block";
     }
   }
+// JavaScript Hecht Bottons
   function rutefunction() {
     var x = document.getElementById("hechtrute");
     if (x.style.display === "block") {
@@ -56,6 +62,67 @@ function divfunction() {
   }
   function zubehoerfunction() {
     var x = document.getElementById("hechtzubehoer");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  //
+  // Scripts für den Zielfisch Karpfe
+  //
+
+  // Karpfenbild
+  function karpfendivfunction() {
+    var x = document.getElementById("karpfendiv");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function karpfenrutefunction() {
+    var x = document.getElementById("karpfenrute");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function karpfenrollefunction() {
+    var x = document.getElementById("karpfenrolle");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function karpfenschnurfunction() {
+    var x = document.getElementById("karpfenschnur");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function karpfenvorfachfunction() {
+    var x = document.getElementById("karpfenvorfach");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function karpfenkoederfunction() {
+    var x = document.getElementById("karpfenkoeder");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function karpfenzubehoerfunction() {
+    var x = document.getElementById("karpfenzubehoer");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
